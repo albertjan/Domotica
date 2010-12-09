3e74a2fcb4164371806fb7c4701c924da4cffac1
Albert-Jan <albertjan@curit.com>
2010-12-09 21:27:26 +0100

    just trying

--- MIP/Interfaces/IEndpoint.cs
+++ MIP/Interfaces/IEndpoint.cs
@@ -2,6 +2,7 @@ namespace MIP.Interfaces
 {
     public interface IEndpoint
     {
+        IEndpointState State { get; set; }
         IConnection ConnectedTo { get; set; }
         string Name { get; set; }
     }
