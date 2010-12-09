a5dd0bcac3d87ee6721658f33e0dab31f67690d3
Albert-Jan <albertjan@curit.com>
2010-12-09 22:47:31 +0100

    Halfarsed start of the implementation of IOberver<>

---
+++ HAL/InputObserver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HAL
+{
+    public class InputObserver : IObserver<IInputTask>
+    {
+        public void OnNext(IInputTask value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
