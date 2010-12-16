d4b0aea7c655c6ae0581595d7cf2eebb90126b69
Albert-Jan <albertjan@curit.com>
2010-12-16 22:21:57 +0100

    Some thoughts on how to loosly couple the hardware to the contolling interfaces

--- HAL/IHardwareController.cs
+++ HAL/IHardwareController.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MIP.Interfaces;
 
 namespace HAL
 {
     public interface IHardwareController
     {
         event EventHandlers.EndpointEventHandler EndpointStateChanged;
+
+        void CoupleEndpoints(IEnumerable<IEndpoint> endpoints, EndPointCouplingInformation endPointCouplingInformation);
+
+        IEnumerable<IHardwareEndpointIndentifiers> GetIdentifiers();
     }
 }
