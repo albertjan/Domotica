1c9238888c569cb30e17dadf1f24f53768e31b16
Albert-Jan <albertjan@curit.com>
2011-02-10 00:27:51 +0100

    Hub Rename

---
+++ MIPLIB/Hubs/SimpleStaticHub.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using MIP.Interfaces;
+
+namespace MIPLIB.Hubs
+{
+    public class SimpleStaticHub : IHub
+    {
+        public SimpleStaticHub()
+        {
+            RegisteredConnections = new List<IConnection>();
+
+        }
+
+        public IList<IConnection> RegisteredConnections { get; set; }
+    }
+}
